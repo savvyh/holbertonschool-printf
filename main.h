3ea9a6f59b925f8e896a9b9bd2467b24af3e539f
savvyh <7277@holbertonstudents.com>
2023-11-21 17:33:39 +0000

    fix the main.h file with a new prototype about a new function that we create

--- main.h
+++ main.h
@@ -3,5 +3,7 @@
 
 int _strlen(char *string);
 char *print_char(const char *format, char argument);
+char *replace_string(char *format, char *argument);
+char *replace_percent(char *format);
 
 #endif
