84ef818d8ea615c289b4e31da2051aad6118872e
Hache-David <hache.david@free.fr>
2023-11-21 10:13:48 +0000

    Add main.h which contains all prototypes function. Add also a basic function _strlen that I create. Well, add print_char.c which draft the pattern %c of the function printf

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef __PRINTF__
+#define __PRINTF__
+
+int _strlen(char *string);
+char *print_char(const char *format, char argument);
+
+#endif
---
+++ print_char.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_char - function to print a character when it match witch the symbol %
+ * follow by the character 'c'.
+ * If it match with %c, then it replaces by the variable 'argument'.
+ *
+ * @format: The input, a string.
+ * @argument: The character to add to the string inside format.
+ *
+ * Return: The string which is the modified format string.
+*/
+
+char *print_char(const char *format, char argument)
+{
+	char *string_return = NULL;
+	int index;
+	int stop = 0;
+	int lenght_argument = 1;
+
+	string_return = malloc(sizeof(format) + (lenght_argument - 2));
+
+	for (index = 0; format[index]; index++)
+	{
+		if (format[index] == '%' && stop != 1)
+		{
+			string_return[index] = argument;
+			index++;
+			stop = 1;
+		}
+		else
+		{
+			string_return[index] = format[index];
+		}
+	}
+	return (string_return);
+}
