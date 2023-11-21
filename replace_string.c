3048aa0f8b74dee80e0f5fb99e5e5d69e8b04d2d
savvyh <7277@holbertonstudents.com>
2023-11-21 15:45:06 +0000

    fix betty for the file that create a function that replace the string %s within format, with argument

---
+++ replace_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * replace_string - Replace the string %s within format, with argument.
+ *
+ * Description: this function searches all occurrence of the string %s
+ *				within the 'format' string and replace with the 'argument' string.
+ *
+ * @format: A string which contains the specifier %s to replace it.
+ * @argument: The string that replace the %s.
+ *
+ * Return: A new string where all %s occurrences have been replaced
+ *			by the value of 'argument'
+*/
+char *replace_string(char *format, char *argument)
+{
+	int format_length = _strlen(format);
+	int argument_length = _strlen(argument);
+	int result_length = format_length + argument_length;
+	int index_format;
+	int index_argument;
+	int index_result = 0;
+
+	char *string_return = NULL;
+
+	string_return = malloc(sizeof(char) * (result_length + 1));
+
+	if (string_return == NULL)
+	{
+		return (NULL);
+	}
+
+	for (index_format = 0; index_format < format_length; index_format++)
+	{
+		if (format[index_format] == '%' && format[index_format + 1] == 's')
+		{
+			for (index_argument = 0; index_argument < argument_length; index_argument++)
+			{
+				string_return[index_result++] = argument[index_argument];
+			}
+			index_format++;
+		}
+		else
+		{
+			string_return[index_result++] = format[index_format];
+		}
+	}
+
+	string_return[index_result] = '\0';
+
+	return (string_return);
+}
