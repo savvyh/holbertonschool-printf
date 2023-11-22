3ee000fcafa06f98923e4c5da32b4e765e61c744
savvyh <7277@holbertonstudents.com>
2023-11-22 14:29:05 +0000

    fix the main.h file with a modification on a prototype function

--- main.h
+++ main.h
@@ -5,7 +5,7 @@ int _strlen(char *string);
 char *format_char(const char *format, char argument);
 char *function_analyser(const char *format, char *string_final,
 						char arguments);
-char *format_percent(char *format);
-char *format_string(char *format, char *argument);
+char *format_percent(const char *format);
+char *format_string(const char *format, char *argument);
 
 #endif
