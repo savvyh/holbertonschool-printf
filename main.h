6fd9b9dd79bde19bc9d6de16003674929913eb45
Hache-David <hache.david@free.fr>
2023-11-23 09:11:50 +0000

    Fix main.h to github issue again

--- main.h
+++ main.h
@@ -5,19 +5,10 @@
 int _strlen(char *string);
 char *format_char(const char *format, char ch);
 char *function_analyser(const char *format, char *string_final,
-<<<<<<< HEAD
 						va_list args_value);
 char *format_percent(const char *format);
 char *format_string(const char *format, char *argument);
 int _putchar(char c);
 int _printf(const char *format, ...);
-char *format_int(const char *format, char entier);
-char *function_int_analyser(const char *format,
-							 char *string_final, va_list args_value);
-=======
-						char arguments);
-char *format_percent(const char *format);
-char *format_string(const char *format, char *argument);
->>>>>>> 3ee000fcafa06f98923e4c5da32b4e765e61c744
 
 #endif
