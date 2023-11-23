9cd40bfa6801d3e6fa015096630379a34bfb6ec5
savvyh <7277@holbertonstudents.com>
2023-11-23 14:32:43 +0000

    fix betty style for this file

--- itoa.c
+++ itoa.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <stdlib.h>
-
-char* _itoa(int value)
+/**
+ * _itoa - function similar to the real function itoa
+ *
+ * Description: the aim of this function is to convert int to char
+ *
+ * @value: the value to convert
+ *
+ * Return: the result of the convertion int to char
+*/
+char *_itoa(int value)
 {
-    int n = abs(value);
-    int num_digits = 1 + (value < 0);
-	   char* result = (char*)malloc((num_digits + 1) * sizeof(char));
+	int n = abs(value);
+	int num_digits = 1 + (value < 0);
+	char *result = (char *)malloc((num_digits + 1) * sizeof(char));
 
 	if (result == NULL)
 	{
-        return NULL;
-    }
+		return (NULL);
+	}
 
-    while (n >= 10) {
-        n /= 10;
-        num_digits++;
-    }
+	while (n >= 10)
+	{
+		n /= 10;
+		num_digits++;
+	}
 
-    n = abs(value);
-    result[num_digits] = '\0';
-    while (num_digits--) {
-        result[num_digits] = (n % 10) + '0';
-        n /= 10;
-    }
+	n = abs(value);
+	result[num_digits] = '\0';
+	while (num_digits--)
+	{
+		result[num_digits] = (n % 10) + '0';
+		n /= 10;
+	}
 
-    if (value < 0) {
-        result[0] = '-';
-    }
-    return result;
+	if (value < 0)
+	{
+		result[0] = '-';
+	}
+	return (result);
 }
