36cf1e9dca3d6b1674c229dfc0d4061fd6b72931
Hache-David <hache.david@free.fr>
2023-11-23 09:03:29 +0000

    fix github issue

--- main.h
+++ main.h
@@ -1,11 +1,17 @@
 #ifndef __PRINTF__
 #define __PRINTF__
+#include <stdarg.h>
 
 int _strlen(char *string);
-char *format_char(const char *format, char argument);
+char *format_char(const char *format, char ch);
 char *function_analyser(const char *format, char *string_final,
-						char arguments);
-char *format_percent(char *format);
-char *format_string(char *format, char *argument);
+						va_list args_value);
+char *format_percent(const char *format);
+char *format_string(const char *format, char *argument);
+int _putchar(char c);
+int _printf(const char *format, ...);
+char *format_int(const char *format, char entier);
+char *function_int_analyser(const char *format,
+							 char *string_final, va_list args_value);
 
 #endif
