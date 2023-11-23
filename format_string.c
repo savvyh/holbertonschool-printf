6c642f21824b0d3498578c6135b01292ab74292f
savvyh <7277@holbertonstudents.com>
2023-11-23 12:32:30 +0000

    fix betty style in this file

--- format_string.c
+++ format_string.c
@@ -6,7 +6,8 @@
  * Description: this function searches all occurrence of the string %s
  *				within the 'format' string and replace with the 'argument' string.
  *
- * @format: A string which contains the specifier %s to replace it.
+ * @string_final: input of a string in format and
+ *				  become the new string with modification.
  * @str: The string that replace the %s.
  *
  * Return: A new string where all %s occurrences have been replaced
@@ -17,8 +18,7 @@ char *format_string(char *string_final, char *str)
 	int format_length = _strlen((char *)string_final);
 	int argument_length = _strlen(str);
 	int result_length = format_length + argument_length;
-	int index_format;
-	int index_argument;
+	int index_format, index_argument;
 	int index_result = 0;
 	int stop = 0;
 
@@ -31,12 +31,12 @@ char *format_string(char *string_final, char *str)
 
 	for (index_format = 0; index_format < format_length; index_format++)
 	{
-		if (string_final[index_format] == '%' && string_final[index_format + 1] == 's' && stop != 1)
+		if (string_final[index_format] == '%'
+			&& string_final[index_format + 1] == 's' && stop != 1)
 		{
 			for (index_argument = 0; index_argument < argument_length; index_argument++)
-			{
 				string_return[index_result++] = str[index_argument];
-			}
+
 			index_format++;
 			stop = 1;
 		}
